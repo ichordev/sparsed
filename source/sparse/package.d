a5d4947bc860b70627066b6ce695cb910b75f4dd
zxin <15670465+ichordev@users.noreply.github.com>
2024-05-05 18:20:05 +1000

    Add copyright notice

--- source/sparse/package.d
+++ source/sparse/package.d
@@ -1,3 +1,9 @@
+/+
++               Copyright 2024 Aya Partridge
++ Distributed under the Boost Software License, Version 1.0.
++     (See accompanying file LICENSE_1_0.txt or copy at
++           http://www.boost.org/LICENSE_1_0.txt)
++/
 module sparse;
 
 ///The smallest integer that can fully index a sparse array with size `indices`.
